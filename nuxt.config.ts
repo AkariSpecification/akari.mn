305c973b69858542b26e33e6278e094cc7f287f4
Takahashi Akari <akaritakahashioss@gmail.com>
2023-04-24 20:28:30 +0900

    2023年 4月24日 月曜日 20時28分30秒 JST

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -21,12 +21,12 @@ export default defineNuxtConfig({
         },
         { property: "og:url", content: "https://akarispecification.github.io/akari.mn/akari.mn" },
         { property: "og:type", content: "website" },
-        { property: "og:title", content: "AkariSpecification" },
-        { property: "og:description", content: "AkariSpecification" },
+        { property: "og:title", content: "AkariSpecification(Port Intel x86-64 intrinsic function to RISC-V or ARM)" },
+        { property: "og:description", content: "Port Intel x86-64 intrinsic function to RISC-V or ARM" },
         {
           property: "og:site_name",
           content:
-            "AkariSpecification - Port Intel x86-64 intrinsic function to RISC-V or ARM",
+            "AkariSpecification(Port Intel x86-64 intrinsic function to RISC-V or ARM)",
         },
         { property: "og:image", content: "https://akarispecification.github.io/akari.mn/images/title.png" },
       ],
