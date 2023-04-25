ff261fccadd6b285a149d62e6d52edad6d7d112a
Takahashi Akari <akaritakahashioss@gmail.com>
2023-04-25 17:05:54 +0900

    2023年 4月25日 火曜日 17時05分54秒 JST

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -21,6 +21,27 @@ export default defineNuxtConfig({
             type: "image/x-icon",
             href: "/favicon.ico",
         },
+        {
+            rel: "apple-touch-icon",
+            sizes: "180x180",
+            href: "/apple-touch-icon.png",
+        },
+        {
+            rel: "icon",
+            type: "image/png",
+            sizes: "32x32",
+            href: "/favicon-32x32.png",
+        },
+        {
+            rel: "icon",
+            type: "image/png",
+            sizes: "16x16",
+            href: "/favicon-16x16.png",
+        },
+        {
+            rel: "manifest",
+            href: "/site.webmanifest",
+        }
     ],
       meta: [
         { charset: "utf-8" },
@@ -54,6 +75,14 @@ export default defineNuxtConfig({
           content:
             "https://akarispecification.github.io/akari.mn/images/title.png",
         },
+        {
+          name: "msapplication-TileColor",
+          content: "#da532c",
+        },
+        {
+          name: "theme-color",
+          content: "#ffffff",
+        }
       ],
     },
   },
