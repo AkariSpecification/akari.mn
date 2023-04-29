fe0a4aef45e9c583fb65c0252697d4f69fed362c
Takahashi Akari <akaritakahashioss@gmail.com>
2023-04-29 14:54:28 +0900

    2023年 4月29日 土曜日 14時54分28秒 JST

--- pages/index.vue
+++ pages/index.vue
@@ -41,15 +41,6 @@ export default defineComponent({
       headers: headers,
     });
     this.infomation = await res.json();
-    if (this.infomation) {
-      const date = new Date(this.infomation.date);
-      const year = date.getFullYear();
-      const month = ("0" + (date.getMonth() + 1)).slice(-2);
-      const day = ("0" + date.getDate()).slice(-2);
-      const hour = ("0" + date.getHours()).slice(-2);
-      const minute = ("0" + date.getMinutes()).slice(-2);
-      this.infomation.date = `${year}/${month}/${day} ${hour}:${minute}`;
-    }
   },
   mounted() {
     Prism.highlightAll();
